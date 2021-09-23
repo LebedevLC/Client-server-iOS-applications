98a4b956638bfd371698a6a54c8a2ef84687cc84
chumovskih_sergey <sergeyka-lalala@yandex.ru>
2021-09-23 18:27:38 +0300

    Home_work_2
    По ДЗ:
    Подключил Alamofire;
    Зарегистрировал приложение в ВК;
    Заменил форму входа на WKWebView для авторизации в ВК;
    Сохранил токен в синглтоне Session;
    Использовал токен в запросах к VK API;
    Реализовал запросы к VK API;
    Получил список друзей;
    Получил фотографии человека;
    Получил группы текущего пользователя;
    Получил группы по поисковому запросу;
    Вывел данные в консоль.
    
    Доработка:
    Добавил возможность масштабировать и двигать изображения в BigImageView (большая картинка новостей) и в BigPhotoView (просмотр изображений друга)
    Добавил анимацию возврата этого изображения в исходное состояние.
    Добавил возможность скрыть/показать навигацию BigImageView и BigPhotoView.
    Добавил возможность вернуться назад свайпом вверх с BigImageView.  

--- VK_application/VK_application/Presentation/Login/LoadingViewController.swift
+++ VK_application/VK_application/Presentation/Login/LoadingViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class LoadingViewController: UIViewController {
     
+    @IBOutlet var animationView: UIView!
+    
     private var firstPoint: UILabel = {
         let firstPoint = UILabel()
         firstPoint.translatesAutoresizingMaskIntoConstraints = false
@@ -49,6 +51,7 @@ class LoadingViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        animationFirst()
         animationSecond()
     }
     
@@ -79,7 +82,6 @@ class LoadingViewController: UIViewController {
     private func animationSecond() {
         UIView.animateKeyframes(withDuration: 1,
                                 delay: 0,
-//                                options: .repeat,
                                 animations: {
                                     UIView.addKeyframe(withRelativeStartTime: 0.15,
                                                        relativeDuration: 0.16,
@@ -114,10 +116,54 @@ class LoadingViewController: UIViewController {
                                 },
                                 completion: { _ in
                                     self.performSegue(withIdentifier: "goToTabBar", sender: nil)
-                                    print("User token = " + UserSession.shared.token)
-                                    print("UserID = " + String(UserSession.shared.userId))
                                 })
     }
     
+    private func animationFirst() {
+        let firstLayer = CAShapeLayer()
+        let secondLayer = CAShapeLayer()
+        
+        firstLayer.path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 4, height: 4)).cgPath
+        secondLayer.path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 4, height: 4)).cgPath
+        
+        firstLayer.backgroundColor = UIColor.black.cgColor
+        secondLayer.backgroundColor = UIColor.black.cgColor
+        
+        firstLayer.frame = CGRect(x: view.center.x - 10, y: view.center.y - 200, width: 40, height: 40)
+        secondLayer.frame = CGRect(x: view.center.x - 10, y: view.center.y - 200, width: 40, height: 40)
+        
+        firstLayer.masksToBounds = true
+        secondLayer.masksToBounds = true
+        firstLayer.cornerRadius = 20
+        secondLayer.cornerRadius = 20
+        
+        self.view.layer.addSublayer(firstLayer)
+        self.view.layer.addSublayer(secondLayer)
+        
+        let scale = CABasicAnimation(keyPath: "bounds.size.width")
+        scale.byValue = 160
+        scale.duration = 0.5
+        scale.fillMode = CAMediaTimingFillMode.forwards
+        scale.isRemovedOnCompletion = false
+        
+        let rotationLeft = CABasicAnimation(keyPath: "transform.rotation")
+        rotationLeft.byValue = CGFloat.pi / 4
+        rotationLeft.duration = 0.5
+        rotationLeft.beginTime = CACurrentMediaTime() + 0.5
+        rotationLeft.fillMode = CAMediaTimingFillMode.both
+        rotationLeft.isRemovedOnCompletion = false
+        
+        let rotationRight = CABasicAnimation(keyPath: "transform.rotation")
+        rotationRight.byValue = -CGFloat.pi / 4
+        rotationRight.duration = 0.5
+        rotationRight.beginTime = CACurrentMediaTime() + 0.5
+        rotationRight.fillMode = CAMediaTimingFillMode.both
+        rotationRight.isRemovedOnCompletion = false
+        
+        firstLayer.add(scale, forKey: nil)
+        firstLayer.add(rotationLeft, forKey: nil)
+        secondLayer.add(scale, forKey: nil)
+        secondLayer.add(rotationRight, forKey: nil)
+    }
     
 }
---
+++ VK_application/VK_application/Web/GroupsSearch.swift
@@ -0,0 +1,49 @@
+//
+//  GroupsSearch.swift
+//  VK_application
+//
+//  Created by Сергей Чумовских  on 22.09.2021.
+//
+
+import Alamofire
+
+class GroupsSearch {
+    /*
+     METHOD - groups.search
+     Cтандартный запрос к API:
+     https://api.vk.com/method/METHOD?PARAMS&access_token=TOKEN&v=V
+     
+     Примерный запрос
+     https://api.vk.com/method/groups.search?q=apple&count=10&access_token=TOKEN&v=5.81
+     
+     q - текст поискового запроса. ОБЯЗАТЕЛЬНЫЙ параметр
+     count - колличество отображаемых групп
+     */
+    
+    func getMyGroups() {
+        // Адресс запроса
+        let urlPath = "https://api.vk.com/method/groups.search"
+        // Параметры запроса
+        let paramters: Parameters = [
+            // поисковый запрос
+            "q": "Apple",
+            // колличество групп
+            "count": "2",
+            // токен доступа
+            "access_token": "\(UserSession.shared.token)",
+            // версия API
+            "v": "5.81"
+        ]
+        
+        AF.request(urlPath, parameters: paramters).responseJSON { response in
+            print("""
+                  ++++++++++++++++++++++++++++++++++++++++++
+                  +++ Starting AF.request SEARCH_GROUPS ++++
+                  ++++++++++++++++++++++++++++++++++++++++++
+                  """)
+            print(response.value as Any)
+            print("+++++++++++END+++++++++++")
+        }
+    }
+    
+}
